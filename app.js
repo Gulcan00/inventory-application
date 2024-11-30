49b5dd6c09bc4b329730dc16c83d62e85b11f29a
Gulcan00 <zipwire1@hotmail.com>
2024-11-30 11:35:21 +0200

    Configure ejs as view engine

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const coffeeRouter = require('./routes/coffeeRouter');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/coffee', coffeeRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
