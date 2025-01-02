d70578e63969dde739fb128d0ab8a66d3efe536d
Gulcan00 <zipwire1@hotmail.com>
2025-01-02 20:37:42 +0200

    Create navbar and generic view to display items.

--- contollers/coffeeController.js
+++ contollers/coffeeController.js
@@ -3,8 +3,9 @@ const db = require('../db/coffeeQueries');
 async function getCoffees(req, res) {
   const items = await db.getCoffees();
 
-  res.render('items', {
+  res.render('items-list', {
     title: 'Coffees',
+    active: 'coffee',
     items,
   });
 }
